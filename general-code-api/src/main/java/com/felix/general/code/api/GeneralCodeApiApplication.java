bd6d87cfe64114c1df7d637a7a55665e7cc2def3
lixin40 <lixin40@kuaishou.com>
2023-08-03 19:29:46 +0800

    initialize the project

---
+++ general-code-api/src/main/java/com/felix/general/code/api/GeneralCodeApiApplication.java
@@ -0,0 +1,15 @@
+package com.felix.general.code.api;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lixin40 <lixin40@kuaishou.com>
+ * Created on 2023-07-30
+ */
+@SpringBootApplication
+public class GeneralCodeApiApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ general-code-daily-practice/src/main/java/com/felix/general/code/daily/practice/GeneralCodeDailyPracticeApplication.java
@@ -0,0 +1,15 @@
+package com.felix.general.code.daily.practice;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lixin40 <lixin40@kuaishou.com>
+ * Created on 2023-07-30
+ */
+@SpringBootApplication
+public class GeneralCodeDailyPracticeApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
